c19dff422d0852a43cb13d2aa7d1e59e981a8332
Anil <anil6996f@gmail.com>
2024-06-24 17:46:24 +0530

    Implemented the setting doc to firebase once the user sign up

--- src/components/SignupSignin/index.js
+++ src/components/SignupSignin/index.js
@@ -3,8 +3,10 @@ import './styles.css';
 import Input from '../Input';
 import Button from '../Button';
 import { toast } from 'react-toastify';
-import { createUserWithEmailAndPassword } from 'firebase/auth';
-import { auth } from '../../firebase';
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth';
+import { auth, db } from '../../firebase';
+import { useNavigate } from 'react-router-dom';
+import { setDoc,doc, getDoc } from 'firebase/firestore';
 
 function SignupSignin () {
   const [name, setName] = useState("");
@@ -12,6 +14,9 @@ function SignupSignin () {
   const [password,setPassword] = useState("")
   const [confirmPassword, setConfirmPassword] = useState("");
   const [loading, setLoading] = useState(false);
+  const [loginForm, setLoginForm] = useState(false);
+
+  const navigate = useNavigate();
 
   function signupWithEmail () {
     setLoading(true);
@@ -29,6 +34,7 @@ function SignupSignin () {
           setPassword('');
           setConfirmPassword('')
           createDoc(user);
+          navigate('/dashboard');
           // ...
         })
         .catch((error) => {
@@ -44,28 +50,96 @@ function SignupSignin () {
     } else {
       toast.error("All fields are mandatory");
       setLoading(false);
-   }
-  }
+    };
+  };
 
-  function createDoc () {
-    //make sure user with the uid does not exist 
+  function loginUsingEmail () {
+    setLoading(true);
+    if (email !== "" && password !== "") {
+       signInWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+        // Signed in 
+          const user = userCredential.user;
+          toast.success("User Logged in");
+          setLoading(false);
+          navigate('/dashboard');
+        // ...
+  })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        setLoading(false);
+        toast.error(errorMessage)
+      });
+    } else {
+      toast.error("All fields are mandatory");
+      setLoading(false);
+    }
+  };
+
+  async function createDoc (user) {
+    setLoading(true);
+    //make sure user with the uid does not exist
     // create a doc
-  }
+
+    if (!user) return;
+
+    const userRef = doc(db, "users", user.uid);
+    const userData = await getDoc(userRef);
+    if (!userData.exists()) {  //creating doc only when the doc does not exist.
+      try {
+      await setDoc(doc(db, "users", user.uid), {
+        name: user.displayName ? user.displayName : name,
+        email:user.email,
+        photoURL: user.photoURL ? user.photoURL : "",
+        createdAt: new Date(),
+      });
+        setLoading(false);
+    } catch (e) {
+        toast.error(e.message);
+        setLoading(false);
+    }
+    } else {
+      toast.error('Doc exists already!')
+      setLoading(false);
+    };
+  };
 
   return (
-    <div className='signup-wrapper'>
-      <h2 className='title'>Sign Up on <span style={{ color: "var(--theme)" }}>FinTrack.</span></h2>
-      <form>
-        <Input label='Full Name' state={name} setState={setName} placeholder='John Doe'/>
-        <Input type='email' label='Email' state={email} setState={setEmail} placeholder='johndoe@gmail.com'/>
-        <Input type='password' label='Password' state={password} setState={setPassword} placeholder='johndoe123' t/>
-        <Input type='password' label='Confirm Password' state={confirmPassword} setState={setConfirmPassword} placeholder='johndoe123' />
-        <Button text={loading ? "Loading..." : 'SignUp using Email and Password'} onClick={signupWithEmail} disabled={loading} />
-        <p style={{textAlign:"center",margin:0}}>or</p>
-        <Button text={loading ? "Loading..." : 'SignUp using Google'} blue={true}/>
-      </form>
-    </div>
-  )
-}
+    <>
+      {loginForm ?
+        <>
+          <div className='signup-wrapper'>
+            <h2 className='title'>Login to  <span style={{ color: "var(--theme)" }}>FinTrack.</span></h2>
+            <form>
+              <Input type='email' label='Email' state={email} setState={setEmail} placeholder='johndoe@gmail.com' />
+              <Input type='password' label='Password' state={password} setState={setPassword} placeholder='johndoe123' t />
+              <Button text={loading ? "Loading..." : 'Login using Email and Password'} onClick={loginUsingEmail} disabled={loading} />
+              <p className='p-login'>or</p>
+              <Button text={loading ? "Loading..." : 'Login using Google'} blue={true} />
+               <p className='p-login' onClick={()=>setLoginForm(!loginForm)}>or Don't Have an account ? Click Here</p>
+            </form>
+          </div>
+        </>
+        :
+        <div className='signup-wrapper'>
+          <h2 className='title'>Sign Up on <span style={{ color: "var(--theme)" }}>FinTrack.</span></h2>
+          <form>
+            <Input label='Full Name' state={name} setState={setName} placeholder='John Doe' />
+            <Input type='email' label='Email' state={email} setState={setEmail} placeholder='johndoe@gmail.com' />
+            <Input type='password' label='Password' state={password} setState={setPassword} placeholder='johndoe123'  />
+            <Input type='password' label='Confirm Password' state={confirmPassword} setState={setConfirmPassword} placeholder='johndoe123' />
+            <Button text={loading ? "Loading..." : 'SignUp using Email and Password'} onClick={signupWithEmail} disabled={loading} />
+            <p className='p-login'>or</p>
+            <Button text={loading ? "Loading..." : 'SignUp using Google'} blue={true} />
+             <p className='p-login' onClick={()=>setLoginForm(!loginForm)}> Have an account already ? Click Here</p>
+          </form>
+        </div>}
+    </>
+  );
+};
 
 export default SignupSignin;
+
+
+//Doc is created at signup only.
